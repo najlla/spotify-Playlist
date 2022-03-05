9530cab68929b3f158831ec4edd664a1d577856a
= <=>
2022-03-05 20:56:16 +0430

    Spotify Playlist

---
+++ main.py
@@ -0,0 +1,12 @@
+from bs4 import BeautifulSoup
+import requests
+date = input("Which year do you want to travel to? Type the date in this format YYYY-MM-DD: ")
+
+response = requests.get(url="https://www.billboard.com/charts/hot-100/" + date)
+soup = BeautifulSoup(response.text, "html.parser")
+song_names_spans = soup.find_all("span", class_="chart-element__information__song")
+song_names = [song.getText() for song in song_names_spans]
+print(song_names)
+
+
+
